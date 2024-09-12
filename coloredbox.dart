b8299b1ef2e4d7a84693449de4ef024e7a5b2768
UrvashiMandovker <urvashi.mandovker1211@gmail.com>
2024-09-12 13:54:15 +0530

    change height

--- coloredbox.dart
+++ coloredbox.dart
@@ -11,7 +11,7 @@ class _ColoredboxState extends State<Coloredbox> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 400,
+      height: 600,
       width: 400,
       color: Colors.redAccent,
     );
